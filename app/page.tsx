02622badb88fbf3b79087f93d7205ae10d43d683
Simon Mueller <simon.mueller2@outlook.de>
2026-04-01 10:40:23 +0200

    changed token usage source in root/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { getToken } from "@/lib/api";
+import { getToken } from "@/lib/auth";
 
 export default function Home() {
   const router = useRouter();
